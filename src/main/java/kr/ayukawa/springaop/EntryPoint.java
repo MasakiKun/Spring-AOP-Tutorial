4c90233b439d787a1cc5dfa1bb192f0b21190812
MasakiKun <kidconan@gmail.com>
2019-03-10 16:20:14 +0900

    불필요 import 제거

--- src/main/java/kr/ayukawa/springaop/EntryPoint.java
+++ src/main/java/kr/ayukawa/springaop/EntryPoint.java
@@ -6,10 +6,7 @@ import kr.ayukawa.springaop.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
-
-import java.util.Map;
 
 @Component
 public class EntryPoint {
